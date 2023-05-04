8d530dd4a6149e53cf6baba327e582e546aa4168
Chemaclass <chemaclass@outlook.es>
2023-05-04 15:01:26 +0200

    Install gacela phpstan-extension

--- src/Shared/CustomerName.php
+++ src/Shared/CustomerName.php
@@ -6,13 +6,10 @@ namespace GacelaModuleExample\Shared;
 
 final class CustomerName
 {
-    private string $firstName;
-    private string $lastName;
-
-    public function __construct(string $firstName, string $lastName)
-    {
-        $this->firstName = $firstName;
-        $this->lastName = $lastName;
+    public function __construct(
+        private string $firstName,
+        private string $lastName
+    ) {
     }
 
     public function getFirstName(): string
